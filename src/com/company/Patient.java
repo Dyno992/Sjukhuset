8a1c5700d76b19b1c35758918b115ec521f2f19b
Dino Mangafic <dino.mangafic@student.kyh.se>
2020-09-28 11:53:42 +0200

    Patient code finished.

--- src/com/company/Patient.java
+++ src/com/company/Patient.java
@@ -1,10 +1,49 @@
 package com.company;
 
-public class Patient {
+class Patient {
 
-    public static void main(String[] args) {
+    private String name;
+    private String sickness;
+    private boolean isSick;
 
-        //code
+    Patient(String name) {
+        this.name = name;
+        this.sickness = "";
+        this.isSick = false;
+    }
+
+    Patient(String name, String sickness) {
+
+        this.name = name;
+
+        if (sickness == null || sickness.equals("")) {
+            this.isSick = false;
+            this.sickness = "";
+        } else {
+            this.sickness = sickness;
+            this.isSick = true;
+
+        }
 
     }
+
+    boolean isSick() {
+        return this.isSick;
+    }
+
+    String getSickness() {
+        return this.sickness;
+    }
+
+    String getName() {
+        return this.name;
+    }
+
+    void takeMedication(Medicine name) {
+        if (this.sickness.equals(name.getTreatmentName())) {
+            this.isSick = false;
+        }
+    }
+
 }
+
